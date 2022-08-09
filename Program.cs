44db97dfa183ea481eed5463c8c84607da8a33c9
Roman Maximov <manul99991@mail.ru>
2022-08-09 20:11:12 +0300

    Решение задания

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] myArray;
+Console.WriteLine("Введите массив строк через пробел:");
+string stroka = Console.ReadLine();
+myArray = stroka.Split(' ');
+
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+void Selection(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        string element = array[i];
+        if (element.Length <= 3)
+        {
+            array[i] = element;
+    }
+        else array[i] = string.Empty;
+    }
+}
+
+Selection(myArray);
+Console.Write("Итоговый массив: ");
+PrintArray(myArray);
